54d17999936b45365a9cfa1fc3ee8fc95fe995e8
NiniLobzhanidze <lobzhanidze264@gmail.com>
2024-11-08 14:52:28 +0400

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char symbol1 = 'A';
+        char symbol2 = 'B';
+
+
+        System.out.println(symbol1 + symbol2);
+
+
+
+
+    }
+}
